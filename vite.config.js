17284198d1ecba83f89066087a4063de5f090fdf
CHENG YANG HO <chengyangho14@gmail.com>
2025-10-19 16:33:29 +0800

    make corrections

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/personal-portfolio/', // Change to your repo name
+  base: '/Personal-Portfolio/', // Change to your repo name
 })
